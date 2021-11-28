e95b2b033a9848d1ad2c3cb5b0dd4fbdbfcab3f8
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-11-28 20:14:30 -0300

    despliegue del frontend en heroku

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/scr/app/components/home'));  // assets/ico/menu.svg
+app.use(express.static(__dirname + '/src/app/components/home'));  // assets/ico/menu.svg
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/home.component.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080); //src\app\components\home\home.component.html
