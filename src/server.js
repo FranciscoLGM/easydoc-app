afe3490da917b3565102ac775181328bfe10c149
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-11-28 20:02:05 -0300

    despliegue del frontend en heroku

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/EasyDoc'));
+app.use(express.static(__dirname + '/app/components/home'));  
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/home.component.html'));
 });
 
 // Start the app by listening on the default Heroku port
