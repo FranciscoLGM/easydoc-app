62469d0bd1fccb66ba8eccbdf2501610e545240a
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-11-29 20:37:46 -0300

    despliegue del frontend en heroku release v37

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/EasyDoc')); // path to the angular app
+app.use(express.static('./dist/EasyDoc')); // path to the angular app
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html', {
-    root: __dirname + './dist/EasyDoc'
+  res.sendFile(path.join('/index.html', {
+    root: './dist/EasyDoc'
   }));
 });
 
