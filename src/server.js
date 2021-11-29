6a9a7b8618c110d8693b84e60041c2661985fdb1
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-11-29 09:21:34 -0300

    despliegue del frontend en heroku Relase v33

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/EasyDoc'));  // path to the angular app
+app.use(express.static(__dirname + '/dist/easy-doc'));  // path to the angular app
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + 'dist/EasyDoc/index.html')); 
+  res.sendFile(path.join(__dirname + '/dist/easy-doc/index.html')); 
 });
 
 // Start the app by listening on the default Heroku port
